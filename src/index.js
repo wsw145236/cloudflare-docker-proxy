6f117a8776fb208b95020855e08136d5f8a9f1ab
wsw145236 <524515844@qq.com>
2026-01-16 14:12:26 +0800

    Update routes to use custom domain 15154415.xyz

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + 15154415.xyz]: dockerHub,
+  ["quay." + 15154415.xyz]: "https://quay.io",
+  ["gcr." + 15154415.xyz]: "https://gcr.io",
+  ["k8s-gcr." + 15154415.xyz]: "https://k8s.gcr.io",
+  ["k8s." + 15154415.xyz]: "https://registry.k8s.io",
+  ["ghcr." + 15154415.xyz]: "https://ghcr.io",
+  ["cloudsmith." + 15154415.xyz]: "https://docker.cloudsmith.io",
+  ["ecr." + 15154415.xyz]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + 15154415.xyz]: dockerHub,
 };
 
 function routeByHosts(host) {
